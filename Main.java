c8d67f2de5842ee43dda6c61e6ba1686c5810644
Jesica <47221127+jesicagimenez94@users.noreply.github.com>
2019-06-21 19:39:39 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Collections;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Vehiculos v1 = new Vehiculos("peugeot", "206", 4, 200000.00);
+        Vehiculos v2 = new Vehiculos("honda", "titan", "125c", 60000.00);
+        Vehiculos v3 = new Vehiculos("peugeot", "208", 5, 250000.00);
+        Vehiculos v4 = new Vehiculos("yamaha", "ybr", "160c", 80500.00);
+
+        List<Vehiculos> vehiculo = new ArrayList<>();
+        vehiculo.add(v1);
+        vehiculo.add(v2);
+        vehiculo.add(v3);
+        vehiculo.add(v4);
+
+        Collections.sort(vehiculo);
+
+
+        for (Vehiculos elemento : vehiculo) {
+            System.out.println(elemento);
+        }
+
+        if (v1.compareTo(v2) < 0 && v1.compareTo(v3) < 0 && v1.compareTo(v4) < 0) {
+            System.out.println("Vehiculo mas barato: " + v1.getMarca()+" "+v1.getModelo());
+        }else if(v2.compareTo(v1) < 0 && v2.compareTo(v3) < 0 && v2.compareTo(v1) < 0){
+            System.out.println("Vehiculo mas barato: " + v2.getMarca()+" "+v2.getModelo());
+        }else if(v3.compareTo(v1) < 0 && v3.compareTo(v2) < 0 && v3.compareTo(v4) < 0){
+            System.out.println("Vehiculo mas barato: " + v3.getMarca()+" "+v3.getModelo());
+        }else if(v4.compareTo(v1) < 0 && v4.compareTo(v2) < 0 && v4.compareTo(v3) < 0){
+            System.out.println("Vehiculo mas barato: " + v4.getMarca()+" "+v4.getModelo());
+        }
+
+        if (v1.compareTo(v2) > 0 && v1.compareTo(v3) > 0 && v1.compareTo(v4) >0) {
+            System.out.println("Vehiculo mas caro: " + v1.getMarca()+" "+v1.getModelo());
+        }else if(v2.compareTo(v1) > 0 && v2.compareTo(v3) > 0 && v2.compareTo(v1) > 0){
+            System.out.println("Vehiculo mas caro: " + v2.getMarca()+" "+v2.getModelo());
+        }else if(v3.compareTo(v1) > 0 && v3.compareTo(v2) > 0 && v3.compareTo(v4) > 0){
+            System.out.println("Vehiculo mas caro: " + v3.getMarca()+" "+v3.getModelo());
+        }else if(v4.compareTo(v1) > 0 && v4.compareTo(v2) > 0 && v4.compareTo(v3) > 0){
+            System.out.println("Vehiculo mas caro: " + v4.getMarca()+" "+v4.getModelo());
+        }
+    }
+}
---
+++ Vehiculos.java
@@ -0,0 +1,76 @@
+package com.company;
+
+public class Vehiculos implements Comparable<Vehiculos> {
+    String marca;
+    String modelo;
+    String cilindrada;
+    int puertas;
+    double precio;
+
+    public Vehiculos(String marca, String modelo, int puertas, double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.puertas = puertas;
+        this.precio = precio;
+    }
+
+    public Vehiculos(String marca, String modelo, String cilindrada, double precio) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.cilindrada = cilindrada;
+        this.precio = precio;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public void setMarca(String marca) {
+        this.marca = marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getCilindrada() {
+        return cilindrada;
+    }
+
+    public void setCilindrada(String cilindrada) {
+        this.cilindrada = cilindrada;
+    }
+
+    public int getPuertas() {
+        return puertas;
+    }
+
+    public void setPuertas(int puertas) {
+        this.puertas = puertas;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+
+
+    public String toString(){
+        return "marca: "+marca+" modelo: "+modelo+" puertas: "+puertas+" cilindrada: "+cilindrada+" precio: "+precio;
+    }
+
+    @Override
+    public int compareTo(Vehiculos v){
+        return this.getPrecio()>v.getPrecio()?1:this.getPrecio()<v.getPrecio()?-1:0;
+
+
+    }
+}
