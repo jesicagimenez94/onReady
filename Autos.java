c723c7bdb6bd3b39fe5f1110001596ce486f0eed
Jesica <47221127+jesicagimenez94@users.noreply.github.com>
2019-06-22 06:26:53 -0300

    Update Autos.java

--- Autos.java
+++ Autos.java
@@ -1,1 +1,12 @@
 
+public class Autos extends Vehículos{
+	public Autos(String ma, String mo, int pu, double pr) {
+		super(n, t, pu, pr);
+	}
+
+	@Override
+	public void mostrarDetalles() {
+		System.out.println("Marca : "+this.getMarca()+" /// "+"Modelo : "+this.getModelo()+" /// " +"Puertas : "+this.getPuertas()+" /// " +"Precio : "+this.getPrecio());
+
+	}
+}
