d498d8eed997f319f3e22b6ef13e306faf726492
Jesica <47221127+jesicagimenez94@users.noreply.github.com>
2019-06-22 06:31:59 -0300

    Update Motos.java

--- Motos.java
+++ Motos.java
@@ -1,1 +1,11 @@
+public class Motos  extends Vehículos{
+	public Autos(String ma, String mo, String  c, double pr) {
+		super(ma, mo, c , pr );
+	}
 
+	@Override
+	public void mostrarDetalles() {
+		System.out.println("Marca : "+this.getMarca()+" /// "+"Modelo : "+this.getModelo()+" /// " +"Cilindrada : "+this.getCilindrada()+" /// " +"Precio : "+this.getPrecio());
+
+	}
+}
